496789122c3ae7adc5c6e8e5fad27e53e01ec90d
ethan910 <ethanwu1008@gmail.com>
2023-10-14 14:38:48 +0800

    First Commit

---
+++ Assets/animation_scirpt.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animation_scirpt : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    public Animator animator;
+    public float speed = 2.0f;
+    public float rotationSpeed =75.0f;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       float translation = Input 
+    }
+}
